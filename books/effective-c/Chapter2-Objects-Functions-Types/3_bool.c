3c1dd72849caf40367f7d33d465e7edc8b73185b
Kavian <kavianam@gmail.com>
2026-07-10 15:29:51 +0330

    Add a C bool file example

---
+++ books/effective-c/Chapter2-Objects-Functions-Types/3_bool.c
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+
+int main(void) {
+    _Bool flag1 = 0;  // _Bool can only store 0 and 1
+    bool flag2 = false;  // bool can use true and false. true and false are macros.
+    return 0;
+}
