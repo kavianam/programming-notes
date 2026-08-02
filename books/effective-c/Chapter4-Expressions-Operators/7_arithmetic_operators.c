b800284425dafe8b406ba4f7b4d7b4b940573983
Kavian <kavianam@gmail.com>
2026-08-02 22:14:23 +0330

    Add C arithmetic operators file example

---
+++ books/effective-c/Chapter4-Expressions-Operators/7_arithmetic_operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    // Logical Negation Operator
+    printf("!1: %d\n", !1);
+    printf("!2: %d\n", !2);
+    printf("!0: %d\n", !0);
+    printf("!-2: %d\n", !-2);
+
+    printf("--------------------------\n");
+
+    printf("10 / 3: %d\n", 10 / 3);
+    printf("-10 / 3: %d\n", -10 / 3);
+    printf("10 / -3: %d\n", 10 / -3);
+    printf("10 / 3: %f\n", 10 / 3);
+    printf("10 / 3.0: %d\n", 10 / 3.0);
+    printf("10 / 3.0: %f\n", 10 / 3.0);
+}
